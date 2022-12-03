1265703b933e00dc15f3ce6c0a3a912a63902376
arsabyaneh <63109912+arsabyaneh@users.noreply.github.com>
2022-12-03 11:40:44 +0100

    added Ordering.Application layer: added contracts, added CQRS features using MediatR

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ordering.Application.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, string key) : base($"Entity \"{name}\" ({key}) not found!")
+        {
+
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException() : base("One or more validation failure has been occured!")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            Errors = failures
+                .GroupBy(x => x.PropertyName, x => x.ErrorMessage)
+                .ToDictionary(group => group.Key, group => group.ToArray());
+        }
+
+        public IDictionary<string, string[]> Errors { get; }
+    }
+}
